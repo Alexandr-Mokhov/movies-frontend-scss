698a45fd9ad28ed6aab03bfdb89a3265d71a13e9
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-08-18 18:29:15 +0500

    refactor: из Header вынести повторяющиеся значения в переменные

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
-import './Header.css';
 
 export default function Header({ loggedIn }) {
   const { pathname } = useLocation();
