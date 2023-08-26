07cc883792745d2096cd0f5f4f93b5fe987cd457
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-08-26 20:51:03 +0500

    refactor: переделать CSS в компоненте MovieCardList на SCSS

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,6 +1,5 @@
 import { useLocation } from 'react-router-dom';
 import MoviesCard from '../MoviesCard/MoviesCard';
-import './MoviesCardList.css';
 
 export default function MoviesCardList({
   foundMovies,
